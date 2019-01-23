4a1c6afd5cc5b9b372905e4b12b7bd9c847ee305
tracygooo <tracygooo@gmail.com>
2019-01-23 16:09:28 -0500

    Computed individual gi and pi Computed group ggj and gpj

--- carry-lookahead-adder/cla.c
+++ carry-lookahead-adder/cla.c
@@ -40,40 +40,44 @@ int bin1[ bits ];
 int bin2[ bits ];
 
 //Character array of inputs in hex form
-char* hex1=NULL;
-char* hex2=NULL;
+//char* hex1=NULL;
+//char* hex2=NULL;
+char hex1[ digits ] ;
+char hex2[ digits ] ;
 
 int ConvertHexToBinary( const char * hex , int * bin ) ;
+int ComputeGiPi() ;
+int PrintArray( const int * arr  , const int arr_size , const char * arr_name ) ;
 
 int main( int argc , char ** agrv ) {
-    /*
-    char buffer[ digits ] ;
-    int n ;
-    n = 0 ;
-    while( scanf( "%[^\n]%*c" , buffer ) == 1 ) {
-        n++ ;
-        printf( "Line%d: %s\n" , n , buffer ) ;
-    }
-    */
-
-    /***************************************************
-     * Read hex numbers into buffers
-     **************************************************/
-    char buffer1[ digits ] ;
-    char buffer2[ digits ] ;
-    if( scanf( "%s %s" , buffer1 , buffer2 ) != 2 ) {
+    
+    /***********************************************************
+     * Add 0 to the begining of hex array
+     * Read numbers from redirected file to array hex1 and hex2  
+     ***********************************************************/
+    hex1[ 0 ] = '0' ;
+    hex2[ 0 ] = '0' ;
+    if( scanf( "%s %s" , hex1 + 1 , hex2 + 1 ) != 2 ) {
         printf( "Number of input hex is not 2...\n") ;
         return EXIT_FAILURE ;
     }
-    printf( "Buffer1: %s\n" , buffer1 ) ;
-    printf( "Buffer2: %s\n" , buffer2 ) ;
+    printf( "hex1: %s\n" , hex1 ) ;
+    printf( "hex2: %s\n" , hex2 ) ;
 
-    int bin[ 12 ] ;
-    ConvertHexToBinary( "FA0" , bin ) ;
+    /***************************************************
+     * Convert hex to binary
+     * Revert binary 
+     **************************************************/
+    ConvertHexToBinary( hex1 , bin1 ) ;
+    ConvertHexToBinary( hex2 , bin2 ) ;
     int j ;
-    for( j = 0 ; j < 12 ; j++ ) {
-        printf( "bin[%d] = %d\n" , j , bin[j] ) ;
+    printf( "bin1:" ) ;
+    for( j = 0 ; j < bits ; j++ ) {
+        printf( "%d" , bin1[j] ) ;
     }
+
+    ComputeGiPi() ;
+    ComputeGgjGpj() ;
 
     return EXIT_SUCCESS ;
 }
@@ -81,84 +85,116 @@ int main( int argc , char ** agrv ) {
 int ConvertHexToBinary( const char * hex , int * bin ) {
     int i ;
     int j ;
-    int h_len ;
-    int b_len ;
-    h_len = strlen( hex ) ;
-    b_len = h_len * 4 ;
-    j = b_len - 1 ;
+    j = bits - 1 ;
 
-    for( i = 0 ; i < h_len ; i++ ) {
+    for( i = 0 ; i < digits ; i++ ) {
         switch( hex[ i ] ) {
             case '0' :
                 bin[ j-- ] = 0 ; bin[ j-- ] = 0 ; bin[ j-- ] = 0 ; bin[ j-- ] = 0 ;
                 break ;
-
             case '1' :
                 bin[ j-- ] = 0 ; bin[ j-- ] = 0 ; bin[ j-- ] = 0 ; bin[ j-- ] = 1 ;
                 break ;
-
             case '2' :
                 bin[ j-- ] = 0 ; bin[ j-- ] = 0 ; bin[ j-- ] = 1 ; bin[ j-- ] = 0 ;
                 break ;
-
             case '3' :
                 bin[ j-- ] = 0 ; bin[ j-- ] = 0 ; bin[ j-- ] = 1 ; bin[ j-- ] = 1 ;
                 break ;
-
             case '4' :
                 bin[ j-- ] = 0 ; bin[ j-- ] = 1 ; bin[ j-- ] = 0 ; bin[ j-- ] = 0 ;
                 break ;
-
             case '5' :
                 bin[ j-- ] = 0 ; bin[ j-- ] = 1 ; bin[ j-- ] = 0 ; bin[ j-- ] = 1 ;
                 break ;
-
             case '6' :
                 bin[ j-- ] = 0 ; bin[ j-- ] = 1 ; bin[ j-- ] = 1 ; bin[ j-- ] = 0 ;
                 break ;
-
             case '7' :
                 bin[ j-- ] = 0 ; bin[ j-- ] = 1 ; bin[ j-- ] = 1 ; bin[ j-- ] = 1 ;
                 break ;
-
             case '8' :
                 bin[ j-- ] = 1 ; bin[ j-- ] = 0 ; bin[ j-- ] = 0 ; bin[ j-- ] = 0 ;
                 break ;
-
             case '9' :
                 bin[ j-- ] = 1 ; bin[ j-- ] = 0 ; bin[ j-- ] = 0 ; bin[ j-- ] = 1 ;
                 break ;
-
             case 'A' :
                 bin[ j-- ] = 1 ; bin[ j-- ] = 0 ; bin[ j-- ] = 1 ; bin[ j-- ] = 0 ;
                 break ;
-
             case 'B' :
                 bin[ j-- ] = 1 ; bin[ j-- ] = 0 ; bin[ j-- ] = 1 ; bin[ j-- ] = 1 ;
                 break ;
-
             case 'C' :
                 bin[ j-- ] = 1 ; bin[ j-- ] = 1 ; bin[ j-- ] = 0 ; bin[ j-- ] = 0 ;
                 break ;
-
             case 'D' :
                 bin[ j-- ] = 1 ; bin[ j-- ] = 1 ; bin[ j-- ] = 0 ; bin[ j-- ] = 1 ;
                 break ;
-
             case 'E' :
                 bin[ j-- ] = 1 ; bin[ j-- ] = 1 ; bin[ j-- ] = 1 ; bin[ j-- ] = 0 ;
                 break ;
-
             case 'F' :
                 bin[ j-- ] = 1 ; bin[ j-- ] = 1 ; bin[ j-- ] = 1 ; bin[ j-- ] = 1 ;
                 break ;
-
             default :
                 printf( "Invalid hex number...\n" ) ;
                 return EXIT_FAILURE ;
         }
-
     }
 
     return EXIT_SUCCESS ;
 }
+
+int ComputeGiPi(){ 
+    int i ;
+    for( i = 0 ; i < bits ; i++ ) {
+        //gi[ i ] = bin1[ i ] * bin2[ i ] ;
+        //pi[ i ] = bin1[ i ] + bin2[ i ] - gi[ i ] ;
+        gi[ i ] = bin1[ i ] && bin2[ i ] ;
+        pi[ i ] = bin1[ i ] || bin2[ i ] ; 
+    }
+    PrintArray( gi , bits , "gi" ) ;
+    PrintArray( pi , bits , "pi" ) ;
+    return EXIT_SUCCESS ;
+}
+
+int ComputeGgjGpj(){
+    int j ;
+    int i ;
+    for( j = 0 ; j < ngroups ; j++ ) {
+        i = j * 8 ;
+        ggj[ j ] = gi[ i + 7 ] || (
+                   pi[ i + 7 ] && gi[ i + 6 ] ) || ( 
+                   pi[ i + 7 ] && pi[ i + 6 ] && gi[ i + 5 ] ) || (
+                   pi[ i + 7 ] && pi[ i + 6 ] && pi[ i + 5 ] && gi[ i + 4 ] ) || (
+                   pi[ i + 7 ] && pi[ i + 6 ] && pi[ i + 5 ] && pi[ i + 4 ] && gi[ i + 3 ] ) || (
+                   pi[ i + 7 ] && pi[ i + 6 ] && pi[ i + 5 ] && pi[ i + 4 ] && pi[ i + 3 ] && gi[ i + 2 ] ) || (
+                   pi[ i + 7 ] && pi[ i + 6 ] && pi[ i + 5 ] && pi[ i + 4 ] && pi[ i + 3 ] && pi[ i + 2 ] && gi[ i + 1 ] ) || (
+                   pi[ i + 7 ] && pi[ i + 6 ] && pi[ i + 5 ] && pi[ i + 4 ] && pi[ i + 3 ] && pi[ i + 2 ] && pi[ i + 1 ] && gi[ i ] ) ;
+
+        gpj[ j ] = pi[ i + 7 ] && pi[ i + 6 ] && pi[ i + 5 ] && pi[ i + 4 ] && pi[ i + 3 ] && pi[ i + 2 ] && pi[ i + 1 ] && pi[ i ] ;
+    }
+
+    PrintArray( ggj , ngroups , "ggj" ) ;
+    PrintArray( gpj , ngroups , "gpj" ) ;
+
+    return EXIT_SUCCESS ;
+}
+
+int PrintArray( const int * arr  , const int arr_size , const char * arr_name ) {
+    printf( "\n%s:\n" , arr_name ) ;
+    int i ;
+    for( i = 0 ; i < arr_size ; i++ ) {
+        printf( "%d" , arr[i] ) ;
+    }
+    return EXIT_SUCCESS ;
+}
+
+
+
+
+
+
+
+
