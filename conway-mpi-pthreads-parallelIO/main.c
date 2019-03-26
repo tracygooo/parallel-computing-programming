c24be6d88ff9caa2a51b84b5dd5545646575e990
tracygooo <tracygooo@gmail.com>
2019-03-26 15:28:33 -0400

    Added comments for MPI parallel I/O

--- conway-mpi-pthreads-parallelIO/main.c
+++ conway-mpi-pthreads-parallelIO/main.c
@@ -60,78 +60,72 @@ int main(int argc, char *argv[])
 {
     int mpi_myrank;
     int mpi_commsize;
-    int * buff ;
-    int * buff2 ;
 
+    //-------- Parameters for MPI I/O -----------------
+    int * w_buff ; // buffer for MPI_File_write_at
+    int * r_buff ; // buffer for MPI_File_read_at
     MPI_File fh;
     MPI_Offset offset ;
-    MPI_Offset file_size ;
     MPI_Status status;
+
+    // Number of chunk array in each MPI rank
     int chunk_int_num ;
 
 
-// Example MPI startup and using CLCG4 RNG
+    // Example MPI startup and using CLCG4 RNG
     MPI_Init( &argc, &argv) ;
     MPI_Comm_size( MPI_COMM_WORLD, &mpi_commsize ) ;
     MPI_Comm_rank( MPI_COMM_WORLD, &mpi_myrank ) ;
-    
-// Init 32,768 RNG streams - each rank has an independent stream
+
+    // Init 32,768 RNG streams - each rank has an independent stream
     InitDefault();
-    
-// Note, used the mpi_myrank to select which RNG stream to use.
-// You must replace mpi_myrank with the right row being used.
-// This just show you how to call the RNG.    
+
+    // Note, used the mpi_myrank to select which RNG stream to use.
+    // You must replace mpi_myrank with the right row being used.
+    // This just show you how to call the RNG.    
     printf("Rank %d of %d has been started and a first Random Value of %lf\n", 
-       mpi_myrank, mpi_commsize, GenVal(mpi_myrank));
+            mpi_myrank, mpi_commsize, GenVal(mpi_myrank));
 
     chunk_int_num = ROWS * ROWS / mpi_commsize ;
-    buff = ( int * ) malloc( chunk_int_num * sizeof( int ) ) ;
+
+    // Allocate memory for chunk array, 
+    w_buff = ( int * ) malloc( chunk_int_num * sizeof( int ) ) ;
+
+    // Assign arbitrary values to chunk array, should be replaced by real universe array untimately 
     int i ;
     for( i = 0 ; i < chunk_int_num ; i++ ) {
-        buff[ i ] = mpi_myrank * chunk_int_num + i ;
-        // printf( "%d " , buff[ i ] ) ;
+        w_buff[ i ] = mpi_myrank * chunk_int_num + i ;
     }
 
+    // ----------------------------------------------------------------------------------------------------
+    // Write chunk array from buffer to file "universe.txt"
+    // ----------------------------------------------------------------------------------------------------
     offset = mpi_myrank * chunk_int_num * sizeof( int ) ;
-    //printf( "offset of rank %d: %d" , mpi_myrank , offset ) ;
-    //offset = mpi_myrank * chunk_int_num ;
-    // MPI_File_open( MPI_COMM_WORLD, "universe.txt", MPI_MODE_CREATE | MPI_MODE_WRONLY , MPI_INFO_NULL, &fh ) ;
     MPI_File_open( MPI_COMM_WORLD, "universe.txt", MPI_MODE_RDWR | MPI_MODE_CREATE , MPI_INFO_NULL, &fh ) ;
-    // MPI_File_write_at( fh , offset , buff , chunk_int_num * sizeof( int ) , MPI_INT, &status );
-    MPI_File_write_at( fh , offset , buff , chunk_int_num , MPI_INT, &status );
+    MPI_File_write_at( fh , offset , w_buff , chunk_int_num , MPI_INT, &status );
     MPI_Barrier( MPI_COMM_WORLD );
 
-    buff2 = ( int * ) malloc( chunk_int_num * sizeof( int ) ) ;
-    
+    // ----------------------------------------------------------------------------------------------------
+    // Read chunk array from file to buffer 
+    // ----------------------------------------------------------------------------------------------------
+    r_buff = ( int * ) malloc( chunk_int_num * sizeof( int ) ) ;
+    MPI_File_read_at(fh, offset, r_buff, chunk_int_num , MPI_INT , &status) ;
     MPI_Barrier( MPI_COMM_WORLD );
-    MPI_File_read_at(fh, offset, buff2, chunk_int_num , MPI_INT , &status) ;
-    MPI_Barrier( MPI_COMM_WORLD );
-
     int j ;
     for( j = 0 ; j < mpi_commsize ; j++ ) {
         if( mpi_myrank == j ) {
-            // for( i = 0 ; i < ROWS * ROWS ; i++ ) {
             for( i = 0 ; i < chunk_int_num ; i++ ) {
-                printf( "%d " , buff2[ i ] ) ;
+                printf( "%d " , r_buff[ i ] ) ;
             }
         }
     }
-    
-    /*
-    MPI_Barrier( MPI_COMM_WORLD );
-    if( mpi_myrank == 0 ){
-        MPI_File_get_size( fh , & file_size ) ;
-        printf( "file_size: %ll" , file_size ) ;
-    }
-    */
 
     MPI_File_close( &fh ); 
 
-
-// END -Perform a barrier and then leave MPI
+    // END -Perform a barrier and then leave MPI
     MPI_Finalize();
-    free( buff ) ;
-    free( buff2 ) ;
+    free( w_buff ) ;
+    free( r_buff ) ;
 
     return 0;
 }
