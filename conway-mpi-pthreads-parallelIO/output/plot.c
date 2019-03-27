a4ee6737bd383b0e8f303496197645f22ca9d9fb
tracygooo <tracygooo@gmail.com>
2019-03-26 22:15:41 -0400

    Added gnuplot script and related c file for ploting heatmap

---
+++ conway-mpi-pthreads-parallelIO/output/plot.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    system( "gnuplot -p 'plot.gp'") ;
+
+}
---
+++ conway-mpi-pthreads-parallelIO/output/plot.gp
@@ -0,0 +1,2 @@
+set view map scale 1
+splot 'data.txt' matrix with image
